65a5ce0f607b8428cc8325d2d9f482b36a135c55
Laxmi Narasimha Reddy <glnreddy6@gmail.com>
2018-01-02 16:00:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,21 +3,21 @@ provider "google" {
   project     = "My Project2"
   region      = "us-central1"
 }
-
-resource "google_compute_instance" "default" {
-  name = "test"
+resource "google_compute_instance" "blog" {
+  name         = "default"
   machine_type = "n1-standard-1"
-  zone = "us-central1-b"
+  zone         = "us-central1-a"
 
   disk {
-    image = "debian-7-wheezy-v20160301" // the operative system (and Linux flavour) that your machine will run
+    image = "debian-cloud/debian-8"
   }
- 
+
+  disk {
+    type    = "local-ssd"
+    scratch = true
+  }
 
   network_interface {
     network = "default"
-    access_config {
-      // Ephemeral IP - leaving this block empty will generate a new external IP and assign it to the machine
-    }
   }
 }
