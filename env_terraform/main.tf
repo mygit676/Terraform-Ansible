e28724db1780e892cd3d1a36adb9f1dd06b4b28f
Laxmi Narasimha Reddy <glnreddy6@gmail.com>
2018-01-28 03:44:07 -0500

    Update main.tf

--- env_terraform/main.tf
+++ env_terraform/main.tf
@@ -2,7 +2,9 @@ terraform {
   backend "s3" {}
 }
 
-provider "aws" {}
+provider "aws" {
+   region = "us-east-1"
+}
 resource "aws_instance" "example"  {
         ami = "ami-97785bed"
         instance_type = "t2.micro"
